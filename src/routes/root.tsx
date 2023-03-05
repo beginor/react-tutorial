a09492d9e8c80611a371f6e824f7273775f4eebe
beginor <beginor@qq.com>
2023-03-05 11:02:13 +0800

    synchronizing-urls-to-form-state

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,5 +1,7 @@
+import { useEffect, useRef } from 'react';
 import {
-    Outlet, useLoaderData, Form, redirect, NavLink, useNavigation
+    Outlet, useLoaderData, Form, redirect, NavLink, useNavigation,
+    LoaderFunctionArgs
  } from 'react-router-dom';
 
 import { ContactInfo, getContacts, createContact } from '../contacts';
@@ -9,27 +11,40 @@ export async function action(): Promise<Response> {
     return redirect(`/contacts/${contact.id}/edit`);
 }
 
-export async function loader(): Promise<{ contacts: ContactInfo[] }> {
-    const contacts = await getContacts();
-    return { contacts };
+export async function loader({ request }: LoaderFunctionArgs): Promise<{ contacts: ContactInfo[], q: string | null }> { // eslint-disable-line max-len
+    const url = new URL(request.url);
+    const q = url.searchParams.get('q');
+    const contacts = await getContacts(q as string);
+    return { contacts, q };
 }
 
 export default function Root(): JSX.Element {
-    const { contacts } = useLoaderData() as { contacts: ContactInfo[] };
+    const { contacts, q } = useLoaderData() as { contacts: ContactInfo[], q: string | null };
     const navigation = useNavigation();
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (!inputRef.current) {
+            return;
+        }
+        inputRef.current.value = q as string;
+    }, [q]);
 
     return (
       <>
         <div id="sidebar">
           <h1>React Router Contacts</h1>
           <div>
-            <form id="search-form" role="search">
+            <Form id="search-form" role="search">
               <input
                 id="q"
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
                 name="q"
+                defaultValue={q as string}
+                ref={inputRef}
               />
               <div
                 id="search-spinner"
@@ -40,7 +55,7 @@ export default function Root(): JSX.Element {
                 className="sr-only"
                 aria-live="polite"
               ></div>
-            </form>
+            </Form>
             <Form method="post">
               <button type="submit">New</button>
             </Form>
