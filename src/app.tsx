bb359fed4e739fb4e23249caed3c2c927700e0d2
beginor <beginor@qq.com>
2023-03-05 09:11:10 +0800

    nested routes

--- src/app.tsx
+++ src/app.tsx
@@ -11,17 +11,19 @@ const router = createBrowserRouter([
     {
         path: '/',
         element: <Root />,
-        errorElement: <ErrorPage />
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "contacts/:contactId",
+                element: <Contact />,
+            },
+        ]
     },
-    {
-        path: "contacts/:contactId",
-        element: <Contact />,
-      },
 ]);
 
 export default function App(props: AppProps): JSX.Element {
     return (
-      <div className='page-wrapper app'>
+      <div className='page-wrapper flex-row'>
         <RouterProvider router={router} />
       </div>
     );
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,3 +1,5 @@
+import { Outlet } from 'react-router-dom';
+
 export default function Root(): JSX.Element {
     return (
       <>
@@ -37,7 +39,9 @@ export default function Root(): JSX.Element {
             </ul>
           </nav>
         </div>
-        <div id="detail"></div>
+        <div id="detail">
+          <Outlet />
+        </div>
       </>
     );
   }
