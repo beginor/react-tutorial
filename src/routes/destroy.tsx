774f60ef6084d8c52d565618e25a786556de215c
beginor <beginor@qq.com>
2023-03-05 10:41:59 +0800

    contectual errors

--- src/routes/destroy.tsx
+++ src/routes/destroy.tsx
@@ -2,6 +2,7 @@ import { ActionFunctionArgs, redirect } from 'react-router-dom';
 import { deleteContact } from '../contacts';
 
 export async function action({ params }: ActionFunctionArgs): Promise<Response> {
+    throw new Error('oh dang!');
     await deleteContact(params.contactId as string);
     return redirect('/');
 }
