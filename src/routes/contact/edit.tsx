901d9fb3d2337c3c261d476d2aaf53a209413615
beginor <beginor@qq.com>
2023-03-10 08:36:33 +0800

    fix edit

--- src/routes/contact/edit.tsx
+++ src/routes/contact/edit.tsx
@@ -18,7 +18,7 @@ export async function action(
 }
 
 export default function EditContact(): JSX.Element {
-  const { contact } = useLoaderData() as { contact: ContactModel };
+  const { model } = useLoaderData() as { model: ContactModel };
   const navigate = useNavigate();
   const formRef = useRef<HTMLFormElement>(null);
 
@@ -27,28 +27,28 @@ export default function EditContact(): JSX.Element {
       <p>
         <span>Name</span>
         <input placeholder="First" aria-label="First name" type="text"
-          name="first" defaultValue={contact.first}
+          name="first" defaultValue={model.first}
         />
         <input placeholder="Last" aria-label="Last name" type="text"
-          name="last" defaultValue={contact.last}
+          name="last" defaultValue={model.last}
         />
       </p>
       <label>
         <span>Twitter</span>
         <input type="text" name="twitter" placeholder="@jack"
-          defaultValue={contact.twitter}
+          defaultValue={model.twitter}
         />
       </label>
       <label>
         <span>Avatar URL</span>
         <input placeholder="https://example.com/avatar.jpg"
           aria-label="Avatar URL"
-          type="text" name="avatar" defaultValue={contact.avatar}
+          type="text" name="avatar" defaultValue={model.avatar}
         />
       </label>
       <label>
         <span>Notes</span>
-        <textarea name="notes" defaultValue={contact.notes} rows={6} />
+        <textarea name="notes" defaultValue={model.notes} rows={6} />
       </label>
       <p>
         <button type="submit">Save</button>
